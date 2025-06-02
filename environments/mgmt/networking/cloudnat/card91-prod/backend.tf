d48a6d4d17d1baae8f48518b94c5c404c187c56c
aishwaryar-cloudside <137072334+aishwaryar-cloudside@users.noreply.github.com>
2025-06-02 10:30:15 +0530

    Delete environments/mgmt/networking/cloudnat/card91-prod directory

--- environments/mgmt/networking/cloudnat/card91-prod/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket = "bkt-cs-mgmt-tfstate"
-    prefix = "mgmt/networking/cloudnat/nat-cs-prod-apps-ass1-01"
-  }
-}
--- environments/mgmt/networking/cloudnat/card91-prod/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-project_id          = "c91-prod-workloads"
-region              = "asia-south1"
