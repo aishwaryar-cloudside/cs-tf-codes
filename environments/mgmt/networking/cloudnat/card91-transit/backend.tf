60c5cf05411e3347f55a54edab318cb08e21ff5c
aishwaryar-cloudside <137072334+aishwaryar-cloudside@users.noreply.github.com>
2025-06-02 10:33:25 +0530

    Delete environments/mgmt/networking/cloudnat/card91-transit directory

--- environments/mgmt/networking/cloudnat/card91-transit/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket = "bkt-card91-mgmt-tfstate"
-    prefix = "mgmt/networking/cloudnat/nat-card91-uat-apps-ass1-01"
-  }
-}
--- environments/mgmt/networking/cloudnat/card91-transit/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-project_id          = "card91-mgmt-apps"
-region              = "asia-south1"
